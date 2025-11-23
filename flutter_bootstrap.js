9ad0cc31fc2cbf404fbf45a7b3b2fefd2a05e437
halil gezer <hg_1993@windowslive.com>
2025-11-24 01:44:44 +0300

    yeni raporlama sistemi yapıldı

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3186535816"
+    serviceWorkerVersion: "456381797"
   }
 });
